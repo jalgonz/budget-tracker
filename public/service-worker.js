2737b63ab01b1fcb668365f02e8561ab63e43314
Joshua Gonzalez <jalgonz96@gmail.com>
2021-09-02 20:19:38 -0400

    update to service worker js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/style.css",
+    "/db.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest",
@@ -11,6 +12,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";   
 
+//install
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -19,8 +21,9 @@ self.addEventListener("install", function (evt) {
         })
     );
     self.skipWaiting();
-})
+});
 
+//activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -37,6 +40,7 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
+//fetch
 self.addEventListener("fetch", function (evt) {
     if(evt.request.url.includes("/api/transaction")) {
         console.log("[Service Worker] Fetch (data)", evt.request.url);
@@ -63,4 +67,4 @@ self.addEventListener("fetch", function (evt) {
             });
         })
     )
-}) 
+})
